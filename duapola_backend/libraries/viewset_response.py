9f026567ca5df13e7af2a9cf5b554511fe6fcb0f
Kevin Evander <mcntosh@gmail.com>
2020-01-15 16:49:30 +0800

    update success code to private

--- duapola_backend/libraries/viewset_response.py
+++ duapola_backend/libraries/viewset_response.py
@@ -3,18 +3,19 @@ from http import HTTPStatus
 
 
 class ViewsetJSONRenderer(JSONRenderer):
-
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        success_code = [
+    def __init__(self):
+        self._success_code = [
             HTTPStatus.OK,
             HTTPStatus.CREATED,
             HTTPStatus.NO_CONTENT
         ]
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
         status = renderer_context.get('response').status_code
         response = {
             'status': status,
-            'response': data if status in success_code else [],
-            'error': data if status not in success_code else [],
+            'response': data if status in self._success_code else [],
+            'error': data if status not in self._success_code else [],
         }
         renderer_context.get('response').status_code = HTTPStatus.OK
 
