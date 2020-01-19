f017f09345c1c5e87bc13178c04c1e39a0cadd69
Kevin Evander <mcntosh@gmail.com>
2020-01-19 13:27:20 +0800

    user module

--- duapola_admin/urls.py
+++ duapola_admin/urls.py
@@ -16,4 +16,36 @@ urlpatterns = [
         views.DashboardView.as_view(),
         name='admin_dashboard'
     ),
+
+    # User URLs
+    path(
+        'user/data',
+        views.user.UserDataView.as_view(),
+        name='user_data'
+    ),
+    path(
+        'user',
+        views.user.UserListView.as_view(),
+        name='user_index'
+    ),
+    path(
+        'user/create',
+        views.user.UserCreateView.as_view(),
+        name='user_create'
+    ),
+    path(
+        'user/<slug:pk>/update',
+        views.user.UserUpdateView.as_view(),
+        name='user_update'
+    ),
+    path(
+        'user/<slug:pk>/password',
+        views.user.UserPasswordChangeView.as_view(),
+        name='user_password'
+    ),
+    path(
+        'user/<slug:pk>/delete',
+        views.user.UserDeleteView.as_view(),
+        name='user_delete'
+    ),
 ]
